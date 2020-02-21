38a4155ae072a367afd84e329f001f926851e1d1
MarceloDaLuz <ma_celinho18@hotmail.com>
2020-02-21 17:32:27 -0300

    converting from binary to decimal

--- index.js
+++ index.js
@@ -1,21 +1,43 @@
 //  get the numbers passed in args
 var numbers = process.argv.slice(2);
+
 function validateNumber(n){
-    if(n.length > 8){
+    if(n.length > 8){        
         return false;
     }else{
+        digits =  n.split("");
+        digitsValidated = [];
+        digits.forEach(digit => {
+            if(parseInt(digit) != 0 && parseInt(digit) != 1){
+                digitsValidated.push(false);
+            }else{
+                digitsValidated.push(true);
+            }
+        });
+        if(digitsValidated.filter(digit => digit == false).length > 0){
+            return false
+        }
+        
         return true;
     }    
 }
 
+function convertToDec(binArr){
+    let decResult = 0;
+    
+    //get the binary and reverse it! 
+    binArr.reverse();
+    binArr.forEach((val, index) => decResult = decResult + (val * Math.pow(2, index)));
+    return decResult;
+}
 numbers.forEach( n => {
-    validateResult = validateNumber(n);
-    console.log(n, validateResult);
-    if(validateNumber === true){
-        console.log("é true");
+    let validated = validateNumber(n);
+    if(validated == true){
+        let valueDec = convertToDec(n.split(""));
+        console.log(`${n} = ${valueDec}`);
     }
-    if(validateNumber === false){
-        console.log("é false");
+    if(validated == false){
+        console.log(`I can not converted ${n} to decimal`)
     }
 });
 
