6682b3e9479fdf65725123cff17044ba06733d5a
MarceloDaLuz <ma_celinho18@hotmail.com>
2020-02-21 18:28:22 -0300

    Refactoring and comments

--- index.js
+++ index.js
@@ -1,35 +1,35 @@
 //  get the numbers passed in args
 var numbers = process.argv.slice(2);
 
+/**
+ * function to validate
+ * @param {string} n - Binary number. E.g.: 1001
+ * @returns {boolean}
+ */
 function validateNumber(n){
     if(n.length > 8){        
         return false;
     }else{
         digits =  n.split("");
-        digitsValidated = [];
-        digits.forEach(digit => {
-            if(parseInt(digit) != 0 && parseInt(digit) != 1){
-                digitsValidated.push(false);
-            }else{
-                digitsValidated.push(true);
-            }
-        });
-        if(digitsValidated.filter(digit => digit == false).length > 0){
-            return false
+        if(digits.filter(digit =>  parseInt(digit) != 0 && parseInt(digit) != 1).length > 0){
+            return false;
         }
         
         return true;
     }    
 }
-
+/**
+ * function to convert binary to decimal
+ * @param {Array} binArr - array with binary numbers. e.g.: 1001 = [1,0,0,1] 
+ * @returns {Number}
+ */
 function convertToDec(binArr){
-    let decResult = 0;
-    
-    //get the binary and reverse it! 
     binArr.reverse();
+    let decResult = 0;
     binArr.forEach((val, index) => decResult = decResult + (val * Math.pow(2, index)));
     return decResult;
 }
+
 numbers.forEach( n => {
     let validated = validateNumber(n);
     if(validated == true){
@@ -40,6 +40,3 @@ numbers.forEach( n => {
         console.log(`I can not converted ${n} to decimal`)
     }
 });
-
-
-
